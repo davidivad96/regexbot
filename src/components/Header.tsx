dea98c11570cb8efbef22fc29ed42aa0e4548904
davidivad96 <david@strivelabs.io>
2024-08-06 15:33:44 +0200

    add regexbot logo to header and favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
 import ArrowTopRightOnSquare from "./ArrowTopRightOnSquareIcon";
 
 type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = () => (
   <div className="flex flex-row justify-between items-center p-4">
-    <h1 className="text-2xl font-semibold text-white">RegexBot</h1>
+    <div className="flex items-center gap-2">
+      <Image
+        src="/regexbot-logo.png"
+        alt="RegexBot Logo"
+        width={50}
+        height={50}
+      />
+      <h1 className="text-2xl font-semibold text-white">RegexBot</h1>
+    </div>
     <a href="https://regexr.com" target="_blank" rel="noreferrer">
       <button className="btn btn-link">
         Go to RegExr
