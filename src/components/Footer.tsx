d0c2e91e99e60cdeadf8e8bfdf8b13ee8710ec9d
davidivad96 <david@strivelabs.io>
2024-08-07 15:49:00 +0200

    add border to header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 type FooterProps = {};
 
 const Footer: React.FC<FooterProps> = () => (
-  <div className="sticky bottom-0 w-full text-center p-4 bg-base-100 z-10">
+  <div className="sticky bottom-0 w-full text-center p-4 bg-base-100 z-10 border-t-[1px] border-t-slate-800">
     <a href="https://x.com/dfodev" target="_blank" rel="noreferrer">
       <h2 className="text-sm font-semibold text-white">Built by @dfodev</h2>
     </a>
