b376d394f50b53ae3536ced21dc90bd56866056c
davidivad96 <david@strivelabs.io>
2024-08-08 16:34:46 +0200

    add producthunt banner

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [{ hostname: "api.producthunt.com" }],
+  },
+};
 
 export default nextConfig;
