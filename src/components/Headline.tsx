c4572eea960c1b32b7c11870648ac7e51d8cbb2e
davidivad96 <david@strivelabs.io>
2024-08-08 15:42:26 +0200

    improve headline text

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -3,7 +3,7 @@ type HeadlineProps = {};
 const Headline: React.FC<HeadlineProps> = () => (
   <div className="flex flex-col gap-4">
     <h1 className="text-5xl font-bold text-center leading-snug">
-      Convert <span className="text-primary">Plain English</span> into{" "}
+      Convert <span className="text-primary">natural language</span> into{" "}
       <span className="text-primary">Powerful RegEx</span> Effortlessly
     </h1>
     <p className="text-xl text-center">
