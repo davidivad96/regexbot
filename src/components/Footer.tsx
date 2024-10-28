c81224f52c2aa7c1889fa063f868a3bb17754365
davidivad96 <david@strivelabs.io>
2024-10-28 12:07:38 +0100

    add RetouchAI link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,13 @@ type FooterProps = {};
 
 const Footer: React.FC<FooterProps> = () => (
   <footer className="sticky bottom-0 w-full text-center p-4 bg-base-100 z-10 border-t-[1px] border-t-slate-800">
-    <a href="https://x.com/dfodev" target="_blank" rel="noreferrer">
-      <h2 className="text-sm font-semibold text-white">Built by @dfodev</h2>
+    <a href="https://x.com/dfodev" target="_blank">
+      <h2 className="text-sm font-semibold text-white">Built by @dfodev:</h2>
+    </a>
+    <a href="https://www.getretouchai.app/" target="_blank">
+      <h2 className="text-sm font-semibold text-white link link-hover">
+        RetouchAI
+      </h2>
     </a>
   </footer>
 );
